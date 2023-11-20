891a8cf384524c49c7618cead6b5ff04b6fb5385
CheniIyes <cheniiyes@gmail.com>
2023-11-20 15:10:03 +0100

    version 0.0.2

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
-  var canvas = document.querySelector('.snake');
-  var canvas2d = canvas.getContext('2d');
+  var canvas = document.querySelector('.snake')
+  var canvas2d = canvas.getContext('2d')
 
-  canvas.width = 700;
-  canvas.height = 600;
+  canvas.width = 700
+  canvas.height = 600
 
-  var snakeParts = [];
-  var snakeLength = 1;
+  var snakeParts = []
+  var snakeLength = 1
+
+  var snakeX = 0
+  var snakeY = 0
+
+  var directionX = 10
+  var directionY = 0
+
+
   var food = []
-
-  var snakeX = 0;
-  var snakeY = 0;
 
   function move() {
     snakeParts.unshift({
@@ -29,19 +34,8 @@ $(document).ready(function () {
     canvas2d.clearRect(0, 0, canvas.width, canvas.height)
     canvas2d.fillStyle = "yellow";
     for (let i = 0; i < snakeParts.length; i++) {
-      canvas2d.fillRect(snakeParts[i].x, snakeParts[i].y, 10, 10);
+      canvas2d.fillRect(snakeParts[i].x, snakeParts[i].y, 10, 10)
 
-    }
-  }
-  function spawnFood() {
-    if (food.length > 10) {
-      var meklaX = Math.floor(Math.random() * canvas.width);
-      var meklaY = Math.floor(Math.random() * canvas.height);
-      food.push({ x: meklaX, y: meklaY });
-    }
-    for (var i = 0; i < food.length; i++) {
-      canvas2d.fillStyle = "red";
-      canvas2d.fillRect(food[i].x, food[i].y, 10, 10);
     }
   }
 
@@ -49,16 +43,15 @@ $(document).ready(function () {
   function gameLoop() {
     move()
     drawSnake()
-    spawnFood()
+    spawnmekla()
+    checkCollision()
     if (!gameEnded) {
-      setTimeout(gameLoop, 100)
+      setTimeout(gameLoop, 100);
     }
   }
 
   setInterval(gameLoop, 100);
 
-  var directionX = 10;
-  var directionY = 0;
   // KeyboardEvent.code
   document.onkeydown = function (event) {
     switch (event.keyCode) {
@@ -81,6 +74,27 @@ $(document).ready(function () {
     }
   }
 
+
+  function spawnmekla() {
+    if (food.length < 10) {
+      var meklaX = Math.floor(Math.random() * canvas.width);
+      var meklaY = Math.floor(Math.random() * canvas.height);
+      food.push({ x: meklaX, y: meklaY });
+    }
+    for (var i = 0; i < food.length; i++) {
+      canvas2d.fillStyle = "red";
+      canvas2d.fillRect(food[i].x, food[i].y, 10, 10);
+    }
+  }
+
+  function checkCollision() {
+    for (var i = 0; i < food.length; i++) {
+      if (snakeX < food[i].x + 10 &&snakeX + 10 > food[i].x &&snakeY < food[i].y + 10 &&snakeY + 10 > food[i].y) {
+        snakeLength++;
+        food.splice(i, 1);
+      }
+    }
+  }
 
 })
 
